3d9401ea92509f9713dc16134c9918d8849e9b5e
harbii <vonnehrvy98@gmail.com>
2022-10-15 10:55:38 +0800

    created findById method in repository interface

--- Group-3-prelims/src/main/java/Group3/Group3prelims/repository/ITicketRepository.java
+++ Group-3-prelims/src/main/java/Group3/Group3prelims/repository/ITicketRepository.java
@@ -1,7 +1,11 @@
 package Group3.Group3prelims.repository;
 
+import com.google.gson.JsonElement;
+
 import Group3.Group3prelims.entity.Ticket;
 
 public interface ITicketRepository {
 	public int save(final Ticket ticket);
+
+	public Ticket findById(final int id);
 }
