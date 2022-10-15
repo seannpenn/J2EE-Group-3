01b5d503a0040b3bab6adca9d3b4b0d2836a437d
Seanpen <sean.pinote@usjr.edu.ph>
2022-10-15 09:27:03 +0800

    Base Project

---
+++ Group-3-prelims/src/main/java/Group3/Group3prelims/Group3PrelimsApplication.java
@@ -0,0 +1,13 @@
+package Group3.Group3prelims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3PrelimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group3PrelimsApplication.class, args);
+	}
+
+}
---
+++ Group-3-prelims/src/test/java/Group3/Group3prelims/Group3PrelimsApplicationTests.java
@@ -0,0 +1,13 @@
+package Group3.Group3prelims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3PrelimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
